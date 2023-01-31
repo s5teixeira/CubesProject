fa68b8c2be01f2bbf6bd00038175965fa335420a
Stephanie Teixeira <steixeira1555@gmail.com>
2023-01-30 23:27:18 -0500

    updated function

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Stephanie Teixeira
+2. This project sends the wufoo data entries to a txt file. It gathers wufoo forms data as entries by using an API key.
+3. Form link: https://stepht15.wufoo.com/forms/z1oe155v174wmjp/
---
+++ main.py
@@ -0,0 +1,54 @@
+import sys
+import requests
+import json
+from secrets import wufoo_key
+from requests.auth import HTTPBasicAuth
+
+subdomain = 'https://stepht15.wufoo.com/api/v3/'
+format = 'forms/cubes-project-proposal-submission/entries/json'
+url = subdomain + format
+username = wufoo_key
+password = 'helloworld'
+wufoo_file = open('txtfile.txt', 'w')
+
+
+def message():
+    response = requests.get(url, auth=(username, password))
+    if response.status_code != 200:
+        print(f"Failed to get data, response code:{response.status_code} and error message: {response.reason} ")
+        print(response.raise_for_status())
+        sys.exit(-1)
+
+
+def get_wufoo_data1():
+    response = requests.get(url, auth=(username, password))
+    data = json.dumps(response.text)
+    print(json.loads(data, indent=4, sort_keys=True))
+
+
+def get_wufoo_data2():
+    response = requests.get(url, auth=(username, password))
+    data = json.loads(response.text)
+    print(json.dumps(data, indent=4, sort_keys=True))
+
+
+def get_wufoo_data3():
+    response = requests.get(url, auth=(username, password))
+    data = json.loads(response.text)
+    wufoo_file.write(str(data))
+    print(json.dumps(data, indent=4, sort_keys=True))
+
+
+def get_wufoo_data4():
+    response = requests.get(url, auth=(username, password))
+    data = json.loads(response.text)
+    wufoo_file.write(str(data))
+    print(json.dumps(data, indent=4, sort_keys=True))
+
+
+if __name__ == '__main__':
+    message()
+    get_wufoo_data1()
+    get_wufoo_data2()
+    get_wufoo_data3()
+    get_wufoo_data4()
