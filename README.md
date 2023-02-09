d3a42c40409287dc63091bf2cf4faf6292e69322
s5teixeira <steixeira1555@gmail.com>
2023-02-09 18:55:10 -0500

    changed readme and put in some test functions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 1. Stephanie Teixeira
-2. This project sends the wufoo data entries to a txt file. It gathers wufoo forms data as entries by using an API key.
-3. Form link: https://stepht15.wufoo.com/forms/z1oe155v174wmjp/
+2. Need pip installed
+3. This project sends the wufoo data entries to a txt file. It gathers wufoo forms data as entries by using an API key. Also gathers data and puts it in a database, and testing.
+4. Used 1 table and manually input my own data into a database.
+5. Wufoo form entries doesnt go into the database
--- test_functions.py
+++ test_functions.py
@@ -1,17 +1,29 @@
 import pytest
 import sqlite3
-import pretendProductionCode
+#import pretendProductionCode
+#cant import pretendproductioncode
+import main
 
 
 def test_show_output():
     with open("test.txt", "w") as test:
-        pretendProductionCode.testable_show_output(1000, .1, test)
+        testable_show_output(1000, .1, test)
     test_file = open("test.txt")
     everything = test_file.readlines()
     str_everything = str(everything)
     assert ' ' in str_everything
 
+#
+# def test_interest():
+#     with pytest.raises(TypeError):
+#         pretendProductionCode.add_interest('too much money', 1)
 
-def test_interest():
-    with pytest.raises(TypeError):
-        pretendProductionCode.add_interest('too much money', 1)
+
+def test_questions_about_data():
+    """Attempted to test on my own data that i put in my database"""
+    with pytest.raises(AssertionError) as exception_info:
+        assert main.make_initial_entries(45) is False
+        assert main.make_initial_entries('Stephanie' is True
+        assert main.make_initial_entries('No') is True
+        assert main.make_initial_entries('Mac') is True
+    assert exception_info is AssertionError
