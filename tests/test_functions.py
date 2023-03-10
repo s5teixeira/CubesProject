6c77e1da7a7561920ab8bf1cc0ac64c87f43787a
s5teixeira <steixeira1555@gmail.com>
2023-03-09 21:51:22 -0500

    flake8 whitespaces

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -3,13 +3,11 @@ import sqlite3
 import getData
 import DatabaseStuff
 import Gui
-
 def test_get_data():
     """ for this test we are just getting the data from wufoo, getting the Entries and counting them"""
     json_data = getData.get_wufoo_data()
     entries = json_data['Entries']
     assert len(entries) <= 10
-
 def test_data_in_database():
     """This function tests to make sure there is data in database table"""
     with pytest.raises(TypeError) as exception_info:
@@ -18,14 +16,12 @@ def test_data_in_database():
         data = c.fethone()
         assert len(data) <= 30
     assert exception_info.type is TypeError
-
 def test_checkboxes():
     """This function tests to see if there are null values(unchecked) from internship table"""
     connection, c = Gui.internship()
     c.execute('SELECT internship FROM WuFooData WHERE internship IS NULL')
     data = c.fethone()
     assert data is None
-
 def test_table_created():
     """There were several ways to do this, some of them include wrapping inserts in try/except blocks,
     but I took an easy way and just check to make sure my table is in the meta deable sqlite_master"""
@@ -35,7 +31,6 @@ def test_table_created():
     record = cursor.fetchone()
     number_of_rows = record[0]  # the number is the first )and only) item in the tuple
     assert number_of_rows == 1
-
 def test_check_text_for_correct_data():
     """This function checks the correct data in the First Name field"""
     with pytest.raises(TypeError) as exception_info:
@@ -48,7 +43,6 @@ def test_check_text_for_correct_data():
         assert test_check == 'johhny'
         assert test_check == 'Test'
     assert exception_info.type is TypeError
-
 def test_check_text_for_correct_data2():
     """This function checks the correct data in the Title field"""
     with pytest.raises(TypeError) as exception_info:
@@ -61,12 +55,10 @@ def test_check_text_for_correct_data2():
         assert test_check == 'House'
         assert test_check == 'ms'
     assert exception_info.type is TypeError
-
 def test_connect_to_db(capfd):
     """this function creates a fake database and makes sure it connects to it"""
     test_db = 'WufooData.db'
     sqlite3.connect(test_db)
-
 def test_user_autofill_data_from_email():
     """This function checks if the autofill function works correctly """
     Gui.entry_first_name.insert(0,)
@@ -76,7 +68,6 @@ def test_user_autofill_data_from_email():
     assert Gui.entry_dept.get() == "CS"
     captured = pytest.stdout.getvalue().strip()
     assert captured == "First name: Steph\n Last name:Teix\n Title: Miss\n Dept: CS\n"
-
 def test_user_creation(user_records):
     """This function tests a user with a duplicate username and verifies that it raises an exception: """
     user1 = ("Steph", " Dr", " Hello1")
@@ -85,7 +76,6 @@ def test_user_creation(user_records):
     user_records.insert(user1)
     with pytest.raises(Exception):
         user_records.insert(user2 + user3)
-
 def test_gui_elements_present():
     """This function checks if there are labels and button widgets"""
     assert isinstance(Gui.label1, Gui.root.Label)
@@ -93,4 +83,3 @@ def test_gui_elements_present():
     assert isinstance(Gui.label_dept, Gui.root.Label)
     assert isinstance(Gui.btn_org, Gui.root.Button)
     assert isinstance(Gui.label_last_name, Gui.root.Label)
-
--- update_database.py
+++ update_database.py
@@ -1,17 +1,13 @@
 import sqlite3
 conn = sqlite3.connect("cubesProject.sqlite")
 c = conn.cursor()
-
 def update_records_from_table():
     """This function prompts users to update the data from the database """
     print("You have selected to update the data from the database :) ")
     update_column = input('Enter the column to update: \n ')
     new_value = input('Enter the new value: \n ')
     condition = input('Enter the condition for the update:    (e.g first_name = "Test") \n ')
-    query = f''' 
-    UPDATE WuFooData 
-    SET {update_column} = ?
-    WHERE {condition} '''
+    query = f''' UPDATE WuFooData SET {update_column} = ? WHERE {condition} '''
     c.execute(query, (new_value,)).fetchone()
     conn.commit()
     conn.close()
